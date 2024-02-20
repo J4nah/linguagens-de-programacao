64427146462aef9829bda1f6d38a0e7f16ace955
Janaína Gomes <jana.gomes90@hotmail.com>
2024-02-19 22:10:17 -0300

    atualizado

--- main.js
+++ main.js
@@ -1,2 +1,41 @@
-window.alert("Hello World!!!")
-console.log("testando....")
+/* 
+alt + shift + a 
+*/
+
+//declarando variaveis
+
+const pi = 3.14; //constante
+
+var x = 5; //variavel global 
+var x = 10;
+
+let nome = "Janaina"; // variavel local
+
+
+console.log(typeof nome);
+
+console.log(typeof pi);
+
+let sobrenome = "Gomes";
+
+console.log(nome + " " + sobrenome) //concatenação
+
+console.log(`${nome} ${sobrenome}`) //interpolação (se der enter ele também irá para a linha de baixo)
+
+function mostrarNome () {
+    console.log("Janaina");
+}
+
+mostrarNome();
+
+function mostrarNome2 (nome) {
+    console.log(nome);
+}
+
+mostrarNome2("Jana");
+
+function soma( a, b) {
+    return a + b;
+}
+
+console.log(soma(5, 5))
