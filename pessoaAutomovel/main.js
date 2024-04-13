0345189362da827be96c5e4a6d9f640585c157c3
Janaína Gomes <jana.gomes90@hotmail.com>
2024-04-13 16:36:57 -0300

    up

--- pessoaAutomovel/main.js
+++ pessoaAutomovel/main.js
@@ -43,8 +43,3 @@ class Main{
     }
 }
 
-Na função addLivroUsuario, você está passando codigo para criar um novo livro, mas codigo não está definido em lugar algum. Provavelmente você quer passar codigoLivro em vez disso.
-Na mesma função addLivroUsuario, você está chamando usuario.addUsuario(usuario) ao invés de usuario.addLivro(livro). Parece que você quer adicionar um livro ao usuário, então a chamada deve ser corrigida.
-No método transferirLivro, você está usando codigo em vez de codigoLivro ao buscar e remover o livro de usuarioOrigem.
-Parece que você está tentando usar uma classe Usuario que não está definida no código fornecido. Você precisará definir essa classe ou garantir que ela seja importada de outro lugar.
-Aqui está o código corrigido:
