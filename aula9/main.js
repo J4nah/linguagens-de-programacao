9c581147c2bcc0dca1834c5a9b067c41e3e8f9ce
Janaína Gomes <jana.gomes90@hotmail.com>
2024-04-22 19:46:48 -0300

    up

---
+++ aula9/main.js
@@ -0,0 +1,14 @@
+let titulo = document.getElementById('titulo');
+console.log(titulo);
+
+//let titulo = document.querySelector('#titulo');
+//console.log(titulo);
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = "Novo Texto";
+
+let button = document.querySelector('.btn');
+console.log(button.innerHTML);
+
+let item2 = document.querySelector('#item2');
+item2.innerHTML = "Morango";
