c10967a90307adb32fac7349a7d9b9e7c694b5be
Janaína Gomes <jana.gomes90@hotmail.com>
2024-04-22 21:48:08 -0300

    up

--- aula9/main.js
+++ aula9/main.js
@@ -1,14 +1,62 @@
-let titulo = document.getElementById('titulo');
+/*let titulo = document.getElementById('titulo');
 console.log(titulo);
 
 //let titulo = document.querySelector('#titulo');
 //console.log(titulo);
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = "Novo Texto";
+//paragrafo.innerHTML = "Novo Texto";
 
 let button = document.querySelector('.btn');
 console.log(button.innerHTML);
 
 let item2 = document.querySelector('#item2');
 item2.innerHTML = "Morango";
+
+function changeImg(){
+
+    document.querySelector('img').src = 'imagens/camel.png';
+
+}
+
+document.querySelector('.btn1').onclick = function(){
+    document.querySelector('audio').play();
+}
+
+document.querySelector('.btn2').onclick = function(){
+    paragrafo.style.color = 'red';
+    paragrafo.style.fontSize = '20px';
+}
+*/
+let listaClientes = [];
+
+function adicionarCliente() {
+    let nome = document.querySelector('#nome').value;
+   
+    listaClientes.push(nome);
+    AtualizarLista();
+}
+
+
+function AtualizarLista() {
+    let lista = document.querySelector('#listaClientes');
+
+
+    lista.innerHTML = '';
+
+
+    listaClientes.forEach((cliente, index) => {
+        lista.innerHTML += `<li id="${index}">${cliente} <button onClick="apagar('${index}')">Apagar</button></li>`;
+    });
+
+
+    document.querySelector('#nome').value = '';
+}
+
+
+function apagar(index) {
+    listaClientes.splice(index, 1);
+
+
+    AtualizarLista();
+}
